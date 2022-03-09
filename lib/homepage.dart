0cffb9f3efc28d4b6055665f381576ed956d76dc
unknown <nabeeha88990@gmail.com>
2022-03-09 15:20:15 -0500

    initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,32 @@
+import 'package:chat_app/chatpage.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChatPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            title: Text("Chats"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group_work),
+            title: Text("Channels"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            title: Text("Profile"),
+          ),
+        ],
+      ),
+    );
+  }
+}
